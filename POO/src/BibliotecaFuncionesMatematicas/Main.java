bc002da6158d1375c7354313ee5180547117bf98
mrosdom <mrosdom1505@g.educaand.es>
2024-11-19 08:20:56 +0100

    ssd

--- POO/src/BibliotecaFuncionesMatematicas/Main.java
+++ POO/src/BibliotecaFuncionesMatematicas/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         FuncionesMatematicas func = new FuncionesMatematicas();
         if(func.esPrimo(7))
-            System.out.println("Primo")
+            System.out.println("Primo");
         else
             System.out.println("No es primo");
     }
