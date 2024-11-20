036f94daf42b4b7bea75ceaf2730d862d371fc9d
mrosdom <mrosdom1505@g.educaand.es>
2024-11-20 14:54:28 +0100

    ssd

--- POO/src/Cubo/Main.java
+++ POO/src/Cubo/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Cubo cubito = new Cubo(5,2);
         Cubo cubazo = new Cubo(5,0);
-        cubito.pinta();
+        cubito.lleno();
+        cubito.vuelcaEn(cubazo);
     }
 }
